f2b57fa3fcd1a4e31fda1ae10c53f0f3fad8ddfa
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-06-05 15:33:16 -0300

    Aula let

---
+++ assets/Js/Aula.js
@@ -0,0 +1,34 @@
+let nome = 'Vinicius'; // String
+let nomes; // Declaro a variavel
+
+nomes = 'Miguel' // Inicialisando Variavel
+
+console.log(nomes)
+
+console.log(nome, 'nasce em 2001.');
+console.log('Em 2003', nome ,'conheceu Sabrina.');
+console.log(nome, 'casou-se com Sabrina.');
+console.log('Sabrina teve 1 filho com' , nome , 'em 2030.');
+console.log('O filho de', nome, 'se chama Miguel.');
+
+
+// Não podemos criar variaveis com palavras reservadas 
+// Variaveis precisam ter nomes significativos
+let nomeCliente = 'João' // Tem um "significado"
+let A = 'joao' // Não faz sentido algum
+
+// Não pode comear o nome de ma variavel com um numero
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+let nomeCompletoDoCliente = 'Vinicius Bueno';
+
+// Case-sensitive
+let nomeCliente = 'Vinicius'
+let nomecliente = 'Bueno'
+
+// Não podemos redeclarar variaveis com let
+let nomeCliente = 'Vinicius'
+nomeCliente = 'Bueno';
+
+// Não utilize var, Utilize let.
+
