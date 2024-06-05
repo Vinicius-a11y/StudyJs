777424ab077378c113ec4ca7eaeb90a1dee97bb9
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-06-05 15:50:22 -0300

    Aula const

---
+++ AulaConst.js
@@ -0,0 +1,9 @@
+// String = Text | Number = numero
+const numberOne = 5; // Number
+const numberTwo = '10'; // String
+const resultado = numberOne * numberTwo;
+const resultDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(numberOne + numberTwo)
