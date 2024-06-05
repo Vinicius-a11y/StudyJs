e725d3b4d77431fd6cfbae881e4af8f49579bad6
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-06-05 20:12:59 -0300

    Let vs Var - Primeira diferença

---
+++ Let_vs_Var-Primeira diferença.js
@@ -0,0 +1,10 @@
+// ECMAScript 2015 (ES6)
+var nome = 'Vinicius'
+var nome = 'Bueno'
+console.log(nome)
+
+
+nome = 'Vinicius' // Não faça isso
+
+let nome = 'Vinicius'
+let nome = 'Bueno'  // Não faça isso pois não pode redeclarar
